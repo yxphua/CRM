e44270382542d8f96691c534db0869a9e56418f5
yxphua <134066886+yxphua@users.noreply.github.com>
2025-12-18 14:30:36 +0800

    Update menu_main.py

--- menu_main.py
+++ menu_main.py
@@ -1,19 +1,25 @@
 import tkinter as tk
+
 from points import open_points_panel
+from pos_system import open_pos_system
 
 def open_menu_main(root):
     from member_menu import open_member_menu
-    for widget in root.winfo_children():
-        widget.destroy()
+    for w in root.winfo_children():
+        w.destroy()
 
     root.title("Dashboard")
-    tk.Label(root, text="The Brew Corner", font=("Arial", 16)).pack(pady=20)
 
-    tk.Button(root, text="Member", width=20,
-              command=lambda: open_member_menu(root)).pack(pady=10)
+    tk.Label(root, text="The Brew Corner", font=("Arial", 16)).pack(pady=10)
 
-    tk.Button(root, text="Points", width=20,
-              command=lambda: open_points_panel(root)).pack(pady=10)
+    tk.Button(root, text="Members", width=25,
+              command=lambda: open_member_menu(root)).pack(pady=5)
 
-    tk.Button(root, text="Logout", width=20,
-              command=lambda: root.destroy()).pack(pady=10)
+    tk.Button(root, text="Points", width=25,
+              command=lambda: open_points_panel(root)).pack(pady=5)
+
+    tk.Button(root, text="POS System", width=25,
+              command=lambda: open_pos_system(root)).pack(pady=5)
+
+    tk.Button(root, text="Logout", width=25,
+              command=root.destroy).pack(pady=10)
